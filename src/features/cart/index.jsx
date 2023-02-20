990378943b7a7d92acd996e01d1e2b82337e3a05
Hasibul294 <hasibulhasan294@gmail.com>
2023-02-20 19:33:32 +0600

    ceate hook and feature

---
+++ src/features/cart/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart = () => {
+  return <div>This is cart feature</div>;
+};
+
+export default Cart;
---
+++ src/features/search/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = () => {
+  return <div>This is Search feature</div>;
+};
+
+export default Search;

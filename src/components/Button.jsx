9b3e56837eaf7d67292c4f0631522bc21290c240
Hasibul294 <hasibulhasan294@gmail.com>
2023-02-20 19:29:42 +0600

    add file

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <div>This is a button component</div>;
+};
+
+export default Button;
---
+++ src/components/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <div>This is form component</div>;
+};
+
+export default Form;
